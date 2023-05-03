1d24900ec5a222fa7ada5018b9b40e3907f5d576
cexybebe <lutik4440@gmail.com>
2023-05-03 16:07:13 +0600

    пипи пупу[3]

--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,4 @@
+from .pages.main_page import MainPage
 from selenium.webdriver.common.by import By
 
 
@@ -8,5 +9,6 @@ def go_to_login_page(browser):
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    go_to_login_page(browser)
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
